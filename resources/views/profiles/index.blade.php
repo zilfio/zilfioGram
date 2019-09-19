4d4bc68370dc9936cfc989169e737fbd2bc21008
zilfio <zilfio88@gmail.com>
2019-09-19 09:47:48 +0200

    Adding Posts to the Database & Many To Many Relationship

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,7 +7,10 @@
             <img src="" class="rounded-circle"/>
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{ $user->username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="#">Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
