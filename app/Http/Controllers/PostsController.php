3a02cfd24574ffea01b5df0f93d9bc55d11d8cdb
zilfio <zilfio88@gmail.com>
2019-09-20 09:17:24 +0200

    Route Model Binding: show the post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -38,5 +38,9 @@ class PostsController extends Controller
 
         return redirect('profile/' . auth()->user()->id);
     }
-    
+
+    public function show(\App\Post $post)
+    {
+        return view("posts.show", compact('post'));
+    }
 }
